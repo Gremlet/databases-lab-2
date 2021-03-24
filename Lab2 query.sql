bfabda4393dc8352bd7e9bce29f9db529820c442
Gremlet <57172450+Gremlet@users.noreply.github.com>
2021-03-24 02:07:36 +0100

    fixes for VG

--- Lab2 query.sql
+++ Lab2 query.sql
@@ -131,28 +131,28 @@ VALUES ('Store','Karlsborgsvägen.20','Oskarshamn',57260,'Sweden'),
 
 
 INSERT INTO  Instock (StoreID, ISBN13, Amount) 
-values (1,9789137154831,10),
-       (2,9789100164218,15),
-       (3,9789178870417,20),
-       (4,9789100185596,24),
-       (5,9781865081816,30)
+values (1,'9789137154831',10),
+       (2,'9789100164218',15),
+       (3,'9789178870417',20),
+       (4,'9789100185596',24),
+       (5,'9781865081816',30)
 
 SELECT * FROM InStock
 
 INSERT INTO InStock
 VALUES 
-    (1, 9789515805195, 25),
-    (3, 9788466329781, 31),
-    (4, 9789177953692, 21),
-    (1, 9789177953685, 19),
-    (2, 9780099591115, 22)
+    (1, '9789515805195', 25),
+    (3, '9788466329781', 31),
+    (4, '9789177953692', 21),
+    (1, '9789177953685', 19),
+    (2, '9780099591115', 22)
 
 SELECT * from BookOrder
 
 INSERT INTO BookOrder
 VALUES
-    (1, 9789100185596, 1 ),
-    (2, 9789177953692, 1 )
+    (1, '9789100185596', 1 ),
+    (2, '9789177953692', 1 )
 
 INSERT INTO Customers
             VALUES (1,'James','Butterburg', 'james@gmail.com', 'Blue Gum Street', 'New York', '70116','United States of America'),
@@ -257,13 +257,164 @@ GROUP BY FirstName, Lastname, Title, Amount
 -- It is important for the store to know which customers have ordered which books, both for record-keeping and making sure the right books go to the right customer.
 
 GO
+-- NEW ADDITIONS START HERE!!
 
-SELECT * FROM [Order records]
+-- make columns in BookAuthor NOT NULL
+
+ALTER TABLE BookAuthor
+ALTER COLUMN ISBN13 NVARCHAR(255) NOT NULL
+
+ALTER TABLE BookAuthor
+ALTER COLUMN AuthorID INT NOT NULL
+
+-- add primary key to BookAuthor
+
+ALTER TABLE BookAuthor
+ADD CONSTRAINT PK_BookAuthor PRIMARY KEY (ISBN13, AuthorID)
+
+-- make BasketID and ISBN13 NOT NULL
+
+ALTER TABLE BookOrder
+ALTER COLUMN BasketID INT NOT NULL
+
+ALTER TABLE BookOrder
+ALTER COLUMN ISBN13 NVARCHAR(255) NOT NULL
+
+-- add primary key to BookOrder
+
+ALTER TABLE BookOrder
+ADD CONSTRAINT PK_BookOrder PRIMARY KEY (BasketID, ISBN13)
+
+-- fix Titles per author view
+-- changed COUNT(BookAuthor.AuthorID) to COUNT(DISTINCT BookAuthor.ISBN13) so it doesn't count the books in each store as separate titles
+
+DROP VIEW [Titles per author]
+
+-- This one is wrong!
+-- GO
+
+-- CREATE VIEW [Titles per author] AS 
+-- SELECT 
+--     CONCAT(FirstName, ' ', LastName) as [Name],
+--     DATEDIFF(Year, Birthday, GETDATE()) as Age,
+--     COUNT(BookAuthor.AuthorID) as Titles,
+--     FORMAT(SUM(InStock.Amount * Books.Price), 'C', 'sv-se' ) as [Stock value]
+    
+-- from Authors
+
+-- INNER JOIN BookAuthor ON BookAuthor.AuthorID = Authors.ID
+-- INNER JOIN Books on Books.ISBN13 = BookAuthor.ISBN13
+-- INNER JOIN InStock ON InStock.ISBN13 = Books.ISBN13
+-- GROUP BY FirstName, LastName, Birthday
+
+GO
+
+-- This one is right!
+CREATE VIEW [Titles per Author] AS
+SELECT 
+    CONCAT(FirstName, ' ', LastName) as [Name],
+    DATEDIFF(Year, Birthday, GETDATE()) as Age,
+    COUNT(DISTINCT BookAuthor.ISBN13) as Titles,
+    FORMAT(SUM(InStock.Amount * Books.Price), 'C', 'sv-se' ) as [Stock value]
+    
+FROM  Authors
+
+INNER JOIN BookAuthor ON BookAuthor.AuthorID = Authors.ID
+INNER JOIN Books on Books.ISBN13 = BookAuthor.ISBN13
+INNER JOIN InStock on Books.ISBN13 = InStock.ISBN13
+GROUP BY FirstName, LastName, Birthday
+
+GO
+
+-- Add all books to all stores in InStock
+
+INSERT INTO InStock
+VALUES
+    (1, '9780099591115', 22), (1, '9781865081816', 18), (1, '9788466329781', 30),
+    (1, '9789100164218', 13), (1, '9789100185596', 21), (1, '9789177953692', 9 ),
+    (1, '9789178870417', 15), (1, '9789178870431', 8), (2, '9781865081816', 20), 
+    (2, '9788466329781', 15), (2, '9789100185596', 10),(2, '9789137154831', 13), 
+    (2, '9789177953685', 22 ), (2, '9789177953692', 21 ), (2, '9789178870417', 18), 
+    (2, '9789178870431', 17 ), (2, '9789515805195', 7), (3, '9780099591115', 30), 
+    (3, '9781865081816', 13), (3, '9789100164218', 27), (3, '9789100185596', 22), 
+    (3, '9789137154831', 10), (3, '9789177953685', 17), (3, '9789177953692', 23), 
+    (3, '9789178870431', 18), (3, '9789515805195', 29), (4, '9780099591115', 11), 
+    (4, '9781865081816', 23), (4, '9788466329781', 22), (4, '9789100164218', 19), 
+    (4, '9789137154831', 9), (4, '9789177953685', 8), (4, '9789178870417', 22), 
+    (4, '9789178870431', 31), (4, '9789515805195', 12), (5, '9780099591115', 15), 
+    (5, '9788466329781', 20), (5, '9789100164218', 21), (5, '9789100185596', 14), 
+    (5, '9789137154831', 12), (5, '9789177953685', 15), (5, '9789177953692', 5), 
+    (5, '9789178870417', 13), (5, '9789178870431', 11), (5, '9789515805195', 7)
 
 
+-- Add to BookOrder so we can demo more than one title in Basket
+
+INSERT INTO BookOrder
+VALUES (1, '9789177953692', 2)
+
+-- Add price to BookOrder so we can get price from here instead of from Books in Order records view 
+
+ALTER TABLE BookOrder
+ADD [Price] money
+
+-- Populate Price column
+
+UPDATE BookOrder
+SET Price = 99 * Amount WHERE ISBN13 = '9789100185596'
+
+UPDATE BookOrder
+SET Price = 165 * Amount WHERE ISBN13 = '9789177953692'
+
+select * from BookOrder
+
+-- Add order date to Basket
+
+ALTER TABLE Basket
+ADD [Order date] DATE
+
+-- Populate Order date
+
+UPDATE Basket 
+SET [Order date] = '2021-01-13' WHERE ID = 1
+
+UPDATE Basket 
+SET [Order date] = '2021-03-15' WHERE ID = 2
+
+UPDATE Basket 
+SET [Order date] = '2021-03-19' WHERE ID = 3
 
 
+SELECT * from [Order records]
 
+-- Making the Order records view better :)
 
+DROP VIEW [Order records]
 
+GO
+CREATE VIEW [Order records] AS
+SELECT 
+    CONCAT(FirstName, ' ', Lastname) as [Customer name],
+    Books.Title,
+    BookOrder.Amount,
+    FORMAT(SUM(BookOrder.Amount * Books.Price), 'C', 'sv-se') as [Total at Current Price],
+    FORMAT(BookOrder.Price/BookOrder.Amount, 'C', 'sv-se') as [Unit price on order date],
+    FORMAT(BookOrder.Price, 'C', 'sv-se') as [Order total],
+    Basket.[Order date] as [Order date]
 
+    
+FROM Customers
+INNER JOIN Basket ON Customers.ID = Basket.CustomerID
+INNER JOIN BookOrder ON Basket.ID = BookOrder.BasketID
+INNER JOIN Books ON Books.ISBN13 = BookOrder.ISBN13
+GROUP BY FirstName, Lastname, Title, Amount, [Order date], BookOrder.Price
+
+GO
+
+-- Adding a few more rows to Basket and BookOrder
+
+INSERT INTO BookOrder
+VALUES 
+    (3, '9781865081816', 2, 200), 
+    (3, '9780099591115', 1, 99), 
+    (2, '9789515805195', 1, 99),
+    (2, '9788466329781', 1, 126)
